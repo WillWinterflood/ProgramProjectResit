96207175a0c99286f793c4a1760bf004cce9cefc
WillWinterflood <146846548+WillWinterflood@users.noreply.github.com>
2024-07-28 13:07:05 +0000

    hello

--- game/island.c
+++ game/island.c
@@ -7,7 +7,8 @@
 #define EXIT_FILE_ERROR 2 //File cannot be read 
 #define EXIT_DATA_ERROR 3 //File is not a valid map 
 
-void checkFile(FILE *file, char *Filename) {
+void checkFile(const char *Filename) {
+    FILE *file;
     
     file = fopen(Filename, "r");
 
@@ -24,33 +25,39 @@ void checkFile(FILE *file, char *Filename) {
     while (fgets(line_buffer, buffer_size, file) != NULL) { 
        
        if (strlen(line_buffer) >= buffer_size - 1) {  //compares the bufferSize and the length of the line to ensure there is no segmentation fault. 
-            printf("Length of line bigger than \n");
+            fprintf(stderr, "Length of line bigger than buffer size \n");
             exit(EXIT_DATA_ERROR);
        }
        int lineLength = strlen(line_buffer);
        width = lineLength;
 
+       if (line_buffer[lineLength - 1] == '\n') {
+            line_buffer[lineLength - 1] == '\0';
+            lineLength--; 
+       }
+
        for (int i = 0; i < lineLength; i++) {
             char ch = line_buffer[i];
             if (ch != 'S' && ch != 'w' && ch != ' ' && ch != 'T' && ch != 'H' && ch != '\n') { //Checks the valid characters in the file.
-                printf("Incorrect chars in this file");
+                fprintf(stderr, "Incorrect chars in this file\n");
                 exit(EXIT_DATA_ERROR);
             }
-       }
+        }
+    
+        printf("%s", line_buffer);
+        
     }
-
+    fclose(file);
 }
 
 int main (int argc, char *argv[]) {
-    char Filename[101];
     FILE *file;
 
     if (argc != 2) {
-        printf("Usage: island <filename>");
+        fprintf(stderr, "Usage: island <filename>");
         return EXIT_ARG_ERROR;  
     }
-
-
-
+    checkFile(argv[1]);
+    return EXIT_SUCCESS;
 
 }
